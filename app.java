cac65c369ab2242015698b4dfc2d5429ea8b269e
Stephanie <stlarar@yahoo.com>
2015-03-23 20:35:43 -0400

    added new classes

--- app.java
+++ app.java
@@ -1,23 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author stlarar
- */
-public class app {
+public class app 
+{
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) 
     {
-        System.out.println("");
+        MyJFrame mjf = new MyJFrame();
         
-        //stephanie larar
     }
     
 }
